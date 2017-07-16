f18fb6146c0ac34aa3af0101beac5bccd2c045d1
Osman Perviz <osmanperviz@gmail.com>
2017-07-16 16:53:52 +0200

    Create test form login end point

---
+++ backend/test/api/login-endpoint.test.js
@@ -0,0 +1,50 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import factory from '../factory';
+import sinon from 'sinon'
+import app from '../../app'
+import AuthService from '../../services/authService.js'
+import APIError from '../../helpers/apiError'
+require('../test-helper')
+
+describe('api/login', () => {
+  var mockObj;
+  var fakeResponse;
+  beforeEach(() => {
+    mockObj = sinon.stub(AuthService, 'login').callsFake(() => fakeResponse);
+  })
+
+  afterEach(function () {
+     mockObj.restore();
+  });
+
+  describe('Success Login', () => {
+    it('should return token on success login', (done) => {
+       fakeResponse = 'Success token'
+       request(app)
+        .post('/api/login')
+        .send({username: 'somme usersnamme', password: 'test password'})
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          expect(res.body.token).to.equal('Success token')
+          done();
+      })
+    })
+  })
+
+  describe('Unsuccessful Login', () => {
+    it('should return error unsuccessful login', (done) => {
+       fakeResponse = Promise.reject(new APIError('some error message', 401, true));
+       request(app)
+        .post('/api/login')
+        .send({username: 'somme usersnamme', password: 'test password'})
+        .set('Accept', 'application/json')
+        .expect(401)
+        .end((err, res) => {
+          expect(res.body.message).to.equal("some error message")
+          done();
+      })
+    })
+  })
+})
